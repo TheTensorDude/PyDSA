d2ddd75d8c06da6d15ea35edd1dcac2f00e34365
Hrithick Sen <hrithicksen1999@gmail.com>
2023-09-06 12:40:49 +0530

    binary search for searching range added

---
+++ binarysearch_range.py
@@ -0,0 +1,24 @@
+# Binary search for searching range in a sorted array
+# Time complexity: O(logn), space complexity: O(1)
+def searchRangeUsingBinarySearch(nums, target):
+    lenNums = len(nums)
+    low, high = 0, lenNums - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if nums[mid] == target:
+            left, right = mid, mid
+            
+            while left - 1 >= 0 and nums[left - 1] == target:
+                left -= 1
+            
+            while right + 1 <= lenNums - 1 and nums[right + 1] == target:
+                right += 1
+                
+            return [left, right]
+            
+        elif nums[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return [-1, -1]
+        
--- kadanesAlgo.py
+++ kadanesAlgo.py
@@ -9,39 +9,3 @@ def kadanesAlgo(arr):
         currSum = max(0, currSum + num)
         maxSum = max(currSum, maxSum)
     return maxSum
-
-
-# Test case 1: Array with all positive elements
-arr1 = [1, 2, 3, 4, 5]
-result1 = kadanesAlgo(arr1)
-# Expected output: 15 (the entire array is the maximum subarray)
-
-# Test case 2: Array with all negative elements
-arr2 = [-5, -4, -3, -2, -1]
-result2 = kadanesAlgo(arr2)
-# Expected output: -1 (choosing the maximum negative value)
-
-# Test case 3: Array with a single element
-arr3 = [7]
-result3 = kadanesAlgo(arr3)
-# Expected output: 7 (the only element is the maximum subarray)
-
-# Test case 4: Array with alternating positive and negative elements
-arr4 = [1, -2, 3, -4, 5, -6]
-result4 = kadanesAlgo(arr4)
-# Expected output: 5 (corresponding to the subarray [5])
-
-# Test case 5: Array with all zeros
-arr5 = [0, 0, 0, 0, 0]
-result5 = kadanesAlgo(arr5)
-# Expected output: 0 (choosing an empty subarray)
-
-# Test case 6: Array with mixed positive and negative elements
-arr6 = [2, -3, 4, -1, -2, 1, 5, -3]
-result6 = kadanesAlgo(arr6)
-# Expected output: 8 (corresponding to the subarray [4, -1, -2, 1, 5])
-
-# Test case 7: Large array with random values
-arr7 = [-2, 1, -3, 4, -1, 2, 1, -5, 4, 7, -4, 3, 2, -1]
-result7 = kadanesAlgo(arr7)
-# Expected output: 13 (corresponding to the subarray [4, -1, 2, 1, -5, 4, 7])
